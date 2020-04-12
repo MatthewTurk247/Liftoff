ceaab01206aa865d13b5a2c372b78f653ab7407c
MatthewTurk247 <mjturk15@gmail.com>
2020-04-12 18:23:28 -0500

    Testing master-detail format.

---
+++ Pods/Target Support Files/Pods-Liftoff-LiftoffUITests/Pods-Liftoff-LiftoffUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Liftoff_LiftoffUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Liftoff_LiftoffUITests
+@end
---
+++ Pods/Target Support Files/Pods-LiftoffTests/Pods-LiftoffTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiftoffTests : NSObject
+@end
+@implementation PodsDummy_Pods_LiftoffTests
+@end
